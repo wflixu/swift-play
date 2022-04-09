5741575dc55760cf03610cf975f4f831d0f324ae
lixu <wflixu@88.com>
2022-04-09 17:10:45 +0800

    init

---
+++ sfui/sfui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sfui
+//
+//  Created by 李旭 on 2022/4/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("VStack Item 1 ")
+            Text("VStack Item 2 ")
+            Text("VStack Item 3 ")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sfui/sfui/sfuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  sfuiApp.swift
+//  sfui
+//
+//  Created by 李旭 on 2022/4/9.
+//
+
+import SwiftUI
+
+@main
+struct sfuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
